90af59dfd49a7e21c2f08b35f7d066253324e895
Davetech101 <enomahog@gmail.com>
2025-10-30 15:00:37 +0100

    Initial commit: Shared components library

---
+++ tailwind-config.ts
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    // This will be extended in each Next.js project
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
