5dc27ba204b0ba060f9978961edcf1dd9e08149c
dev-john88 <john@unifiedbeez.com>
2025-11-08 19:21:10 +0100

    update: expose bg image fetch

--- src/lib/supabase/useSupabase.ts
+++ src/lib/supabase/useSupabase.ts
@@ -19,6 +19,7 @@ export const useSupabaseImages = () => {
       beeImg: getAssetUrl(SUPABASE_IMAGES.beeImg),
       beeBotImg: getAssetUrl(SUPABASE_IMAGES.beeBotImg),
       footerBg: getAssetUrl(SUPABASE_IMAGES.footerBg),
+      bgImage: getAssetUrl(SUPABASE_IMAGES.bgImage),
     }),
     []
   );
