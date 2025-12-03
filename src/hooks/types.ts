fe2fb8cbf09de7522ec8ba5343d9d33bfd2cf057
e3xlabs <exploit@unifiedbeez.com>
2025-12-03 16:42:50 +0100

    create reusable autoscroll hook

---
+++ src/hooks/types.ts
@@ -0,0 +1,9 @@
+
+export interface SmartScrollIntoViewOptions {
+  behavior?: ScrollBehavior;
+  block?: ScrollLogicalPosition;
+  container?: HTMLElement | null;
+  enabled?: boolean;
+  requireFullVisibility?: boolean;
+  rootMargin?: string;
+}
