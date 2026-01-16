7b6bb7d92ea48683a83026144d5b99979cec560f
exploitenomah <iksploiting@gmail.com>
2026-01-16 16:12:54 +0100

    update assets

--- src/components/ui/AppDownloadButtons.tsx
+++ src/components/ui/AppDownloadButtons.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import Image from "next/image";
-import Button from "@shared/components/ui/Button";
-import { useSupabaseIcons } from "@shared/lib/supabase/useSupabase";
-import { cn } from "@shared/lib/utils";
+import Button from "../../components/ui/Button";
+import { useSupabaseIcons } from "../../lib/supabase/useSupabase";
+import { cn } from "../../lib/utils";
 
 interface AppDownloadButtonsProps {
   className?: string;
@@ -16,14 +16,14 @@ export default function AppDownloadButtons({
 
   return (
     <div className={cn("flex gap-2", className)}>
-      <Button className="rounded-lg border border-dark-base-50 flex items-center justify-center hover:bg-black bg-black py-1 px-2 gap-2 transition-transform hover:scale-105">
+      <Button className="rounded-lg border border-dark-base-50 flex items-center justify-center hover:bg-black! bg-black py-1 px-2 gap-2 transition-transform hover:scale-105">
         <Image src={icons.appstore} alt="appstore" width={19} height={21} />
         <span className="text-xs flex flex-col items-start text-left leading-tight">
           <span className="text-[.9rem] text-primary">Download on the</span>
           <span className="text-[1.2rem] text-primary">App Store</span>
         </span>
       </Button>
-      <Button className="rounded-lg border border-dark-base-50 flex items-center justify-center hover:bg-black bg-black py-1 px-2 gap-2 transition-transform hover:scale-105">
+      <Button className="rounded-lg border border-dark-base-50 flex items-center justify-center hover:bg-black! bg-black py-1 px-2 gap-2 transition-transform hover:scale-105">
         <Image src={icons.playstore} alt="playstore" width={19} height={21} />
         <span className="text-xs flex flex-col items-start text-left leading-tight">
           <span className="uppercase text-[.9rem] text-primary">Get it on</span>
