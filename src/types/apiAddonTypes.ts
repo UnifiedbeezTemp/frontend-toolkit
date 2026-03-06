68f10e64ae42f5eeba60ee3926e422665f94e2bf
Davetech101 <enomahog@gmail.com>
2026-03-06 17:19:50 +0100

    addons filter

--- src/types/apiAddonTypes.ts
+++ src/types/apiAddonTypes.ts
@@ -8,6 +8,7 @@ export interface ApiAddon {
   billingInterval?: string; // "MONTHLY" | "YEARLY"
   isActive: boolean;
   maxQuantity: number | null;
+  category?: string; // "USAGE_PACK" | etc.
   createdAt: string;
   updatedAt: string;
 }
